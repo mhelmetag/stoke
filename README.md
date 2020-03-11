fb33fcc07cc29a7ba9e8a92a7b525b9a1c52d38d
Max Helmetag <mhelmetag@gmail.com>
2020-03-10 21:28:50 -0700

    lots of stuff

--- README.md
+++ README.md
@@ -1,68 +1,25 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Stoke
 
-## Available Scripts
+Check in on forecast and prediction archives
 
-In the project directory, you can run:
+## More Info
 
-### `npm start`
+Things and stuff.
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## How to Deploy
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+The static site is hosted on Netlify and deployed via their CD pipeline.
 
-### `npm test`
+## How to Run Locally
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Like any `create-react-app`-based app, use:
 
-### `npm run build`
+```sh
+$ yarn start
+```
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+# Credits
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+Icon made by [Freepik](https://www.flaticon.com/authors/freepik) from [www.flaticon.com](https://www.flaticon.com/).
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Quotes by [Uncle Tito](https://rocketpower.fandom.com/wiki/Tito_Makani).
--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+const { REACT_APP_STOKE_ARCHIVES_URL } = process.env;
 
 function App() {
+  const [spotsLoading, setSpotsLoading] = useState(true);
+  const [spots, setSpots] = useState([]);
+  const [predictionsloading, setPredictionsLoading] = useState(true);
+  const [predictions, setPredictions] = useState([]);
+
+  useEffect(() => {
+    async function fetchSpots() {
+      let data = await fetch(`${REACT_APP_STOKE_ARCHIVES_URL}/spots`)
+        .then(response => response.json())
+        .then(data => data);
+
+      if (data && data.spots) {
+        setSpots(data.spots);
+        setSpotsLoading(false);
+      }
+    }
+
+    async function fetchPredictions() {
+      let todayDate = new Date();
+      let todayDateString = todayDate.toISOString().slice(0, 10);
+
+      let data = await fetch(
+        `${REACT_APP_STOKE_ARCHIVES_URL}/predictions?created_on=${todayDateString}`
+      )
+        .then(response => response.json())
+        .then(data => data);
+
+      if (data && data.predictions) {
+        setPredictions(data.predictions);
+        setPredictionsLoading(false);
+      }
+    }
+
+    if (spotsLoading) {
+      fetchSpots();
+    }
+
+    if (predictionsloading) {
+      fetchPredictions();
+    }
+  }, [spotsLoading, predictionsloading]);
+
+  function predictionsComponent() {
+    if (spotsLoading || predictionsloading) {
+      return <section></section>;
+    } else {
+      let groupedPredictions = {};
+      predictions.forEach(prediction => {
+        groupedPredictions[prediction.spot_id] = [
+          ...(groupedPredictions[prediction.spot_id] || []),
+          prediction
+        ];
+      });
+
+      return (
+        <section>
+          <div className="container">
+            {Object.entries(groupedPredictions).map(([spotId, predictions]) => {
+              let spot = spots.find(s => {
+                return String(s.id) === spotId;
+              });
+
+              return (
+                <div className="card" key={spot.id}>
+                  <div className="card-content level is-mobile">
+                    <div className="level-item has-text-centered">
+                      <div>
+                        <p className="heading">Spot Name</p>
+                        <p className="title">{spot.name}</p>
+                      </div>
+                    </div>
+                    {predictions.map(prediction => {
+                      let predictionDate = new Date(prediction.forecasted_for);
+                      let predictionDateString = predictionDate.toLocaleDateString(
+                        "en-US"
+                      );
+
+                      return (
+                        <div
+                          className="level-item has-text-centered"
+                          key={prediction.id}
+                        >
+                          <div>
+                            <p className="heading">
+                              {predictionDateString} UTC
+                            </p>
+                            <p className="title">
+                              {prediction.surfline_height} Ft
+                            </p>
+                            <p className="title">
+                              {prediction.stoke_height} Ft
+                            </p>
+                            <p className="title">
+                              {prediction.surfline_height -
+                                prediction.stoke_height}{" "}
+                              Ft
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+      );
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section
+        className="section"
+        style={{ background: "linear-gradient(to bottom right, green, blue" }}
+      >
+        <div className="container">
+          <div className="level">
+            <div className="level-left">
+              <div className="level-item">
+                <h1 className="title has-text-white">Stoke</h1>
+              </div>
+            </div>
+            <div className="level-right">
+              <div className="level-item">
+                <a
+                  className="level-item"
+                  href="https://github.com/mhelmetag/stoke"
+                >
+                  <span className="icon is-large">
+                    <i className="fab fa-lg fa-github has-text-white"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {predictionsComponent()}
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>Built by Maxworld Technologies</p>
+        </div>
+      </footer>
+    </>
   );
 }
 
