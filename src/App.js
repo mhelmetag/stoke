4d9fd40fdefa9c3954faa4202f09c56fc330aec5
Max Helmetag <mhelmetag@gmail.com>
2020-03-11 00:48:42 -0700

    columns

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
 
   function predictionsComponent() {
     if (spotsLoading || predictionsloading) {
-      return <section></section>;
+      return <section className="section"></section>;
     } else {
       let groupedPredictions = {};
       predictions.forEach(prediction => {
@@ -58,56 +58,58 @@ function App() {
       });
 
       return (
-        <section>
+        <section className="section">
           <div className="container">
-            {Object.entries(groupedPredictions).map(([spotId, predictions]) => {
-              let spot = spots.find(s => {
-                return String(s.id) === spotId;
-              });
+            <div className="columns is-multiline">
+              {Object.entries(groupedPredictions).map(
+                ([spotId, predictions]) => {
+                  let spot = spots.find(s => {
+                    return String(s.id) === spotId;
+                  });
 
-              return (
-                <div className="card" key={spot.id}>
-                  <div className="card-content level is-mobile">
-                    <div className="level-item has-text-centered">
-                      <div>
-                        <p className="heading">Spot Name</p>
-                        <p className="title">{spot.name}</p>
-                      </div>
-                    </div>
-                    {predictions.map(prediction => {
-                      let predictionDate = new Date(prediction.forecasted_for);
-                      let predictionDateString = predictionDate.toLocaleDateString(
-                        "en-US"
-                      );
+                  return (
+                    <div className="column is-full" key={spot.id}>
+                      <div className="card">
+                        <div className="card-content">
+                          <div className="level">
+                            <div className="level-item has-text-centered">
+                              <div>
+                                <p className="heading">Spot Name</p>
+                                <p className="title">{spot.name}</p>
+                              </div>
+                            </div>
+                            {predictions.map(prediction => {
+                              let predictionDate = new Date(
+                                prediction.forecasted_for
+                              );
+                              let predictionDateString = predictionDate.toLocaleDateString(
+                                "en-US"
+                              );
 
-                      return (
-                        <div
-                          className="level-item has-text-centered"
-                          key={prediction.id}
-                        >
-                          <div>
-                            <p className="heading">
-                              {predictionDateString} UTC
-                            </p>
-                            <p className="title">
-                              {prediction.surfline_height} Ft
-                            </p>
-                            <p className="title">
-                              {prediction.stoke_height} Ft
-                            </p>
-                            <p className="title">
-                              {prediction.surfline_height -
-                                prediction.stoke_height}{" "}
-                              Ft
-                            </p>
+                              return (
+                                <div
+                                  className="level-item has-text-centered"
+                                  key={prediction.id}
+                                >
+                                  <div>
+                                    <p className="heading">
+                                      {predictionDateString} UTC
+                                    </p>
+                                    <p className="title">
+                                      {prediction.stoke_height} Ft
+                                    </p>
+                                  </div>
+                                </div>
+                              );
+                            })}
                           </div>
                         </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
+                      </div>
+                    </div>
+                  );
+                }
+              )}
+            </div>
           </div>
         </section>
       );
@@ -129,10 +131,7 @@ function App() {
             </div>
             <div className="level-right">
               <div className="level-item">
-                <a
-                  className="level-item"
-                  href="https://github.com/mhelmetag/stoke"
-                >
+                <a href="https://github.com/mhelmetag/stoke">
                   <span className="icon is-large">
                     <i className="fab fa-lg fa-github has-text-white"></i>
                   </span>
